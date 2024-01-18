0ef435a466ae41649bf9bf4ace9238d457c96fd2
kadagandlasu <154365009+kadagandlasu@users.noreply.github.com>
2024-01-18 10:04:46 -0800

    Update game.py
    This is the first rough draft of the code.

--- game.py
+++ game.py
@@ -2,24 +2,186 @@ import displayio
 import random
 import terminalio
 from adafruit_display_text import label
-from time import sleep
+import time
 
 game_group = displayio.Group()
 
-cowboy = displayio.OnDiskBitmap()
-desert = displayio.OnDiskBitmap()
+cowboy_sprites = displayio.OnDiskBitmap("western/western.bmp")
+dynamite_sprites = displayio.OnDiskBitmap("western/dynamite.bmp")
+desert = displayio.OnDiskBitmap("western/desert.bmp")
+
+bkgnd = displayio.TileGrid(desert, pixel_shader = desert.pixel_shader)
+
+cowboy1 = displayio.TileGrid(
+    cowboy_sprites, 
+    pixel_shader = cowboy_sprites.pixel_shader,                 
+    width = 32, 
+    height = 32, 
+    tile_width = 6, tile_height = 2
+)
+
+cowboy2 = displayio.TileGrid(
+    cowboy_sprites, 
+    pixel_shader = cowboy_sprites.pixel_shader,
+    width = 32, 
+    height = 32,
+    tile_width = 6, 
+    tile_height = 2
+)
+
+dynamite = displayio.TileGrid(
+    dynamite_sprites, 
+    pixel_shader = dynamite_sprites.pixel_shader,
+    width = 32, 
+    height = 32,
+    tile_width = 4, 
+    tile_height = 1
+)
+
+cowboy1_score = 0
+cowboy2_score = 0
+
+def player_count(p1_button:bool, p2_button:bool):
+    text = "1-PLAYER      2-PLAYER"
+    font = terminalio.FONT
+    color = 0x0000FF
+    text_area = label.Label(font, text = text, color = color)
+    game_group.append(text_area)
+    global cowboy_count
+    if p1_button:
+        cowboy_count = 1
+    elif p2_button:
+        cowboy_count = 2
+    game_group.remove(text_area)
+    
+
+def difficulty(p1_button:bool, p2_button:bool):
+    text = "CASUAL    CHALLENGING"
+    font = terminalio.FONT
+    color = 0x0000FF
+    text_area = label.Label(font, text = text, color = color)
+    game_group.append(text_area)
+    global diff_setting
+    if cowboy_count == 1:
+        if p1_button:
+            diff_setting = "casual"
+        if p2_button:
+            diff_setting "challenging"
+    game_group.remove(text_area)
+    
+
+def comp_react():
+    global initial
+    initial = time.time()
+    global seconds
+    if diff_setting == "casual":
+        seconds == random.randint(2,3)
+    elif diff_setting == "challenging":
+        seconds == random.randint(1,2)
+
+
+def win_animate(cowboy1_win:bool,cowboy2_win:bool):
+    if cowboy1_win == True and cowboy2_win == False:
+        cowboy1[0] = 1
+        time.sleep(0.25)
+        cowboy1[0] = 2
+        time.sleep(0.5)
+        cowboy2[0] = 4
+        dynamite[0] = 2
+        time.sleep(0.25)
+        cowboy2[0] = 5
+        cowboy1[0] = 1
+        time.sleep(0.25)
+        dynamite[0] = 3
+        time.sleep(0.25)
+        cowboy1[0] = 3
+    elif cowboy1_win == False and cowboy2_win == True:
+        cowboy2[0] = 1
+        time.sleep(0.25)
+        cowboy2[0] = 2
+        time.sleep(0.5)
+        cowboy1[0] = 4
+        dynamite[0] = 2
+        time.sleep(0.25)
+        cowboy1[0] = 5
+        cowboy2[0] = 1
+        time.sleep(0.25)
+        dynamite[0] = 3
+        time.sleep(0.25)
+        cowboy2[0] = 3
+
+
+def score(cowboy1_win:bool,cowboy2:bool):
+    global cowboy1_score
+    global cowboy2_score
+    if cowboy1_win == True and cowboy2_win == False:
+        cowboy1_score += 1
+    elif cowboy1_win == False and cowboy2_win == True:
+        cowboy2_score += 1
+    
+
 
 def game_setup():
     """this is called once to initialize your game features"""
-    pass
+    game_group.append(bkgnd)
+    game_group.append(cowboy1)
+    game_group.append(cowboy2)
+    game_group.append(dynamite)
+    
 
 def game_frame(p1_button:bool,p2_button:bool) -> bool:
-    """this is called every frame, you need to update all your grame objects
+    """this is called every frame, you need to update all your game objects
         returns True when the game is over, else return false"""
+    cowboy1[0] = 0
+    cowboy2[0] = 6
+    dynamite[0] = 0
+    if cowboy_count != 1 or cowboy_count != 2:
+        player_count(p1_button,p2_button)
+    if diff_setting != "casual" or diff_setting != "challenging":
+        difficulty(p1_button,p2_button)
+    if cowboy_count == 1:
+        time.sleep(random.randint(2,5))
+        comp_react()
+        dynamite[0] = 1
+        if p1_button:
+            win_animate(True,False)
+            score(True,False)
+        elif time.time() == initial + seconds:
+            win_animate(False, True)
+            score(False,True)
+    elif cowboy_count == 2:
+        time.sleep(random.randint(2,5))
+        dynamite[0] = 1
+        if p1_button:
+            win_animate(True,False)
+            score(True,False)
+        elif p2_button:
+            win_animate(False,True)
+            score(True,False)
+    if cowboy1_score == 2 or cowboy2_score == 2:
+        return True
     return False
 
 
 def game_over():
     """this should display your game over screen with score also clean up the game_group"""
-    pass
-
+    global cowboy1_score
+    global cowboy2_score
+    global diff_setting
+    global player_count
+    
+    game_group.remove(cowboy1)
+    game_group.remove(cowboy2)
+    game_group.remove(dynamite)
+    text = str(cowboy1_score) + " - " + str(cowboy2_score)
+    font = terminalio.FONT
+    color = 0x0000FF
+    text_area = label.Label(font, text = text, color = color)
+    game_group.append(text_area)
+    time.sleep(5)
+    game_group.remove(text_area)
+    game_group.remove(bkgnd)
+    cowboy1_score = 0
+    cowboy2_score = 0
+    diff_setting = ""
+    player_count = -1
